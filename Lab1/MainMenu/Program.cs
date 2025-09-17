ce13b33f7c01a4c8b6e8a729532381d5d713f962
MiraDiv <omega24122004@gmail.com>
2025-09-17 20:28:56 +0300

    After satisfying requirements

---
+++ Lab1/MainMenu/Program.cs
@@ -0,0 +1,59 @@
+﻿// Дико вибачаюсь за джава-стиль
+namespace MainMenu;
+
+public class Program
+{
+    public static void Main()
+    {
+        while (true)
+        {
+            Console.Clear();
+            
+            Console.WriteLine("Неймовірна пригода Лабораторного Гаю"
+                + "\n\tОберіть свій шлях:" 
+                + "\n\t(1) - Двері до щастя (завдання 1)" 
+                + "\n\t(2) - Масивна конструкція (завдання 2)"
+                + "\n\t(3) - Вічне питання (завдання 3)"
+                + "\n\t(4) - Трикутник смерті (завдання 4)"
+                + "\n\t(5) - Загадговий університет (Завдання 5)"
+                + "\n\t(0) - Шлях прокляття (вихід з програми)");
+            Console.Write("Зробіть свій вибір >>> ");
+            
+            string input = Console.ReadLine();
+            
+            switch (input)
+            {
+                case "1":
+                    Task1.Program.Main();
+                    break;
+                
+                case "2":
+                    Task2.Program.Main();
+                    break;
+                case "3":
+                    Task3.Program.Main();
+                    break;
+                case "4":
+                    Task4.Program.Main();
+                    break;
+                case "5":
+                    Task5.Program.Main();
+                    break;
+                    
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Ви на порозі до шляху прокляття. Майте на увазі, назад дороги не буде.");
+                    Console.Write("Продовжити?\n (1 - так, 2 - ні) >>> ");
+                    
+                    string confirm = Console.ReadLine();
+                    
+                    if (confirm == "1")
+                    {
+                        Environment.Exit(0);
+                    }
+                    
+                    break;
+            }
+        }
+    }
+}
---
+++ Lab1/Task2/Program.cs
@@ -0,0 +1,83 @@
+﻿// Дико вибачаюсь за джава-стиль
+namespace Task2;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Clear();
+
+        int[] numbers = GenerateRandomArray(10, 1, 100);
+        
+        Console.WriteLine("Йшовши цією тропою, я натрапив на невідому масивну конструкцію. \nНа ній були цифри:\n");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        
+        Console.WriteLine("\n\nЗрозумівши, що це якась математична комбінація, я почав міркувати, та згодом зробив такі висновки: ");
+        
+        Console.WriteLine($"Сума: {GetSum(numbers)}");
+        Console.WriteLine($"Середнє: {GetAverage(numbers):F2}");
+        Console.WriteLine($"Мінімум: {GetMin(numbers)}");
+        Console.WriteLine($"Максимум: {GetMax(numbers)}");
+        
+        Console.WriteLine("\nАле згодом я прийшов думки, що це було марно...\nКожен раз коли я відвертаюсь, цифри знову змінюються...\n\nBad Ending");
+
+        Console.ReadKey();
+    }
+    
+    public static int[] GenerateRandomArray(int size, int min, int max)
+    {
+        Random random = new Random();
+        int[] array = new int[size];
+        
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = random.Next(min, max + 1);
+        }
+        
+        return array;
+    }
+    
+    public static int GetSum(int[] numbers)
+    {
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        return sum;
+    }
+    
+    public static double GetAverage(int[] numbers)
+    {
+        return (double)GetSum(numbers) / numbers.Length;
+    }
+    
+    public static int GetMin(int[] numbers)
+    {
+        int min = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number < min)
+            {
+                min = number;
+            }
+        }
+        return min;
+    }
+    
+    public static int GetMax(int[] numbers)
+    {
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        return max;
+    }
+}
