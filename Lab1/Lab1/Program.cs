95780043e67c70aa5c33f9194da129b1e59eee2c
MiraDiv <omega24122004@gmail.com>
2025-09-17 13:45:23 +0300

    Final Result (no test)

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace lab1
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                
+                Console.WriteLine("Неймовірна пригода Лабораторного Гаю"
+                    + "\n\tОберіть свій шлях:" 
+                    + "\n\t(1) - Двері до щастя (завдання 1)" 
+                    + "\n\t(2) - Масивна конструкція (завдання 2)"
+                    + "\n\t(3) - Вічне питання (завдання 3)"
+                    + "\n\t(4) - Трикутник смерті (завдання 4)"
+                    + "\n\t(5) - Загадговий університет (Завдання 5)"
+                    + "\n\t(0) - Шлях прокляття (вихід з програми)");
+                Console.Write("Зробіть свій вибір >>> ");
+                
+                string input = Console.ReadLine();
+                
+                switch (input)
+                {
+                    case "1":
+                        Z1.JustDoIt();
+                        break;
+                    
+                    case "2":
+                        Z2.JustDoIt();
+                        break;
+                    case "3":
+                        Z3.JustDoIt();
+                        break;
+                    case "4":
+                        Z4.JustDoIt();
+                        break;
+                    case "5":
+                        Z5.JustDoIt();
+                        break;
+                        
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Ви на порозі до шляху прокляття. Майте на увазі, назад дороги не буде.");
+                        Console.Write("Продовжити?\n (1 - так, 2 - ні) >>> ");
+                        
+                        string confirm = Console.ReadLine();
+                        
+                        if (confirm == "1")
+                        {
+                            Environment.Exit(0);
+                        }
+                        
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Lab1/Lab1/Z3.cs
@@ -0,0 +1,53 @@
+namespace lab1;
+
+public class Z3
+{
+    public static void JustDoIt()
+    {
+        Console.Clear();
+        
+        Console.WriteLine("Обернувшись до цього шляху, переді мною став старець із примітивною дерев'яною милицею.\nВін спитав про мій вік. Варто мені збрехати чи відповісти чесно?");
+        Console.Write("\nСказати вік >>> ");
+        
+        string input = Console.ReadLine();
+        
+        Console.WriteLine("\nСтарець послухав мене, нахиливши голову, ніби з невеликим скепсисом.\nАж потім він сказав: ");
+        int age = int.TryParse(input, out int result) ? result : 0;
+        Console.WriteLine("\"" + ClassifyAge(age) + "\"");
+
+        if (age >= 18 && age < 59)
+        {
+            Console.WriteLine("\n\nGood Ending");
+        }
+        else
+        {
+            Console.WriteLine("\n\nBad Ending");
+        }
+
+        Console.ReadKey();
+    }
+
+    private static string ClassifyAge(int age)
+    {
+        if (age <= 0 || age >= 120)
+        {
+            return "Здається ти мені збрехав. Не може бути такого віку. Ходи геть, брехун!";
+        }
+        else if (age <= 12)
+        {
+            return "Надто малий ти ще для таких пригод, друже. Йди звідси!";
+        }
+        else if (age >= 13 && age < 17)
+        {
+            return "Підліток це добре, але все ще недостатньо. Ходи геть!";
+        }
+        else if (age >= 18 && age < 59)
+        {
+            return "Що ж, ти у гарному віці. Добре, проходь";
+        }
+        else
+        {
+            return "Для таких пригод ти вже надто старий. Не пройдеш ти сюди!";
+        }
+    }
+}
