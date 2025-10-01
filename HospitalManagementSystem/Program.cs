69f4025caf977f8900a34c75866a324a16d76a9e
MiraDiv-git <omega24122004@gmail.com>
2025-10-02 02:16:24 +0300

    Initial

--- HospitalManagementSystem/Program.cs
+++ HospitalManagementSystem/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HospitalManagementSystem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
